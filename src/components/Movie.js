243d90fef94c4b267015498e4ae6e4f1a2e8182d
Tomasz Rudnicki <tomek198821@wp.pl>
2021-06-03 09:53:00 +0100

    security commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const IMG_API = "http://image.tmdb.org/t/p/w1280";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const moviePoster ='http://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80'
+const moviePoster ='https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80'
 
 const setVoteClass = (vote)=>{
   if(vote >=8){
