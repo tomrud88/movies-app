7e5556087922da347077711d9be2cf8204e335e4
Tomasz Rudnicki <tomek198821@wp.pl>
2022-03-12 08:45:23 +0000

    https change

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@
 import React,{useEffect, useState} from 'react'
 import Movie from './components/Movie'
 
-const FEATURED_API = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
 
 
-const SEARCH_API = "http://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
 
